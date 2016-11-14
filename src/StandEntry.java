88ca85fde5220f72dac49b8ebf94fa66ada3842c
jack99walsh <jack99walsh@gmail.com>
2016-11-13 23:07:05 -0500

    added a Main file and finished (I think) StandEntry

--- src/StandEntry.java
+++ src/StandEntry.java
@@ -18,8 +18,8 @@ public final class StandEntry {
 	public int highGoalAttempts, highGoalScoring, lowGoalAttempts, lowGoalScoring;
 	public boolean didScale;
 
-	public StandEntry(final String teamName, final String teamNumber, final boolean drive, final boolean cross,
-									   final boolean scoreHigh, final boolean scoreLow, int portcullis, int cdf, int moat,
+	private StandEntry(final String teamName, final String teamNumber, boolean drive, boolean cross,
+									 	 boolean scoreHigh, boolean scoreLow, int portcullis, int cdf, int moat,
 										 int drawBridge, int roughTerrain, int rockWall, int lowBar, int highGoalAttempts,
 										 int highGoalScoring, int lowGoalAttempts, int lowGoalScoring, boolean didScale){
 											 this.teamName = teamName;
@@ -42,18 +42,25 @@ public final class StandEntry {
 											 this.didScale = didScale;
 										 }
 
+	public StandEntry(final String teamName, final String teamNumber, boolean autoBools[], int scoring[], boolean didScale){
+									 		 this(teamName, teamNumber, autoBools[0], autoBools[1], autoBools[2], autoBools[3], scoring[0], scoring[1],
+											 			scoring[2], scoring[3], scoring[4], scoring[5], scoring[6], scoring[7], scoring[8], scoring[9], scoring[10],
+														didScale);
+										 }
+
 	@Override
 	public String toString(){
-		return String.format(this.teamName,this.teamNumber, this.drive, this.cross, this.scoreHigh, this.scoreLow,
+		return String.format("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s",
+												 this.teamName,this.teamNumber, this.drive, this.cross, this.scoreHigh, this.scoreLow,
 												 this.portcullis, this.cdf, this.moat, this.drawBridge, this.roughTerrain, this.rockWall,
 												 this.lowBar, this.highGoalAttempts, this.highGoalScoring, this.lowGoalAttempts,
 												 this.lowGoalScoring, this.didScale);
 
 	}
 	public static void main(String[] args){
-	//boolean eh[] = {true, true, true, true};
-	//int nums[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-		StandEntry entry = new StandEntry("Voltage", "386", true, true, true, true, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, true);
+	  boolean eh[] = {true, true, true, true};
+	  int nums[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+		StandEntry entry = new StandEntry("Voltage", "386", eh, nums, true);
 		System.out.println(entry);
-	} //FIGURE OUT WHAT THE FUCK IS GOING ON
+	}
 }
