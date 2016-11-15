49dac56cb4faef9a3ca7f5b046cb95acfff9d4ac
jack99walsh <jack99walsh@gmail.com>
2016-11-15 12:58:26 -0500

    Wrote some of the frame code. Needs libraries for Swing

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,10 @@
-public java.util.*;
+import javax.*;
+
+public class Main{
+  public static void main (String[] args){
+    JFrame frame = new JFrame("Hello World Swing!");
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+  }
+}
