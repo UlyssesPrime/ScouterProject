1cb64a08cb3bd22a807874664c0f5b4a4c988fa5
Jack Walsh <jack99walsh@gmail.com>
2016-12-04 15:57:47 -0500

    Backing up for working on my laptop

--- src/PitsGUI.java
+++ src/PitsGUI.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Jack on 2016/12/03.
@@ -11,4 +14,41 @@ public class PitsGUI {
     private JRadioButton scoresHighRadioButton;
     private JCheckBox portcullisCheckBox;
     private JCheckBox chevalDeFriseCheckBox;
+    private JCheckBox moatCheckBox;
+    private JCheckBox drawBridgeCheckBox;
+    private JCheckBox roughTerrainCheckBox;
+    private JCheckBox rockWallCheckBox;
+    private JCheckBox lowBarCheckBox;
+    private JCheckBox scaleCheckBox;
+    private JTextField teamNameTextField;
+    private JTextField teamNumberTextField;
+    private JButton clearButton;
+    private JButton submitButton;
+
+    private boolean nothingSelected = nothingRadioButton.isSelected();
+    private boolean movesSelected = movesRadioButton.isSelected();
+    private boolean crossesSelected = crossesRadioButton.isSelected();
+    private boolean scoresLowSelected = scoresLowRadioButton.isSelected();
+    private boolean scoresHighSelected = scoresHighRadioButton.isSelected();
+    private boolean portcullisSelected = portcullisCheckBox.isSelected();
+    private boolean chevalDeFriseSelected = chevalDeFriseCheckBox.isSelected();
+    private boolean moatSelected = moatCheckBox.isSelected();
+    private boolean drawBridgeSelected = drawBridgeCheckBox.isSelected();
+    private boolean roughtTerrainSelected = roughTerrainCheckBox.isSelected();
+    private boolean rockWallSelected = rockWallCheckBox.isSelected();
+    private boolean lowBarSelected = lowBarCheckBox.isSelected();
+    private boolean scaleSelected = scaleCheckBox.isSelected();
+
+    private String nameField = teamNameTextField.getText();
+    private String numberField = teamNumberTextField.getText();
+
+
+    public PitsGUI() {
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
--- src/StandEntry.java
+++ src/StandEntry.java
@@ -18,18 +18,20 @@ public final class StandEntry {
 	public final int highGoalAttempts, highGoalScoring, lowGoalAttempts, lowGoalScoring;
 	public final boolean didScale;
 
-	private StandEntry(final String teamName, final String teamNumber, boolean drive, boolean cross,
-									 	 boolean scoreHigh, boolean scoreLow, int portcullis, int cdf, int moat,
+	private StandEntry(final String teamName, final String teamNumber, int finalScore, boolean drive, boolean cross,
+									 	 boolean scoreHigh, boolean scoreLow, int portcullis, int cdf, int ramparts, int moat,
 										 int drawBridge, int roughTerrain, int rockWall, int lowBar, int highGoalAttempts,
 										 int highGoalScoring, int lowGoalAttempts, int lowGoalScoring, boolean didScale){
 											 this.teamName = teamName;
 											 this.teamNumber = teamNumber;
+                                             this.finalScore = finalScore;
 											 this.drive = drive;
 											 this.cross = cross;
 											 this.scoreHigh = scoreHigh;
 											 this.scoreLow = scoreLow;
 											 this.portcullis = portcullis;
 											 this.cdf = cdf;
+                                             this.ramparts = ramparts;
 											 this.moat = moat;
 											 this.drawBridge = drawBridge;
 											 this.roughTerrain = roughTerrain;
@@ -38,28 +40,28 @@ public final class StandEntry {
 											 this.highGoalAttempts = highGoalAttempts;
 											 this.highGoalScoring = highGoalScoring;
 											 this.lowGoalAttempts = lowGoalScoring;
-											 this.lowGoalScoring = lowGoalScoring;
+											 this.lowGoalScoring = lowGoalAttempts;
 											 this.didScale = didScale;
 										 }
 
 	public StandEntry(final String teamName, final String teamNumber, boolean autoBools[], int scoring[], boolean didScale){
-									 		 this(teamName, teamNumber, autoBools[0], autoBools[1], autoBools[2], autoBools[3], scoring[0], scoring[1],
-											 			scoring[2], scoring[3], scoring[4], scoring[5], scoring[6], scoring[7], scoring[8], scoring[9], scoring[10],
-														didScale);
+									 		 this(teamName, teamNumber, scoring[0], autoBools[0], autoBools[1], autoBools[2], autoBools[3], scoring[1], scoring[2],
+											 			scoring[3], scoring[4], scoring[5], scoring[6], scoring[7], scoring[8], scoring[9], scoring[10], scoring[11],
+                                                        scoring[12], didScale);
 										 }
 
 	@Override
 	public String toString(){
-		return String.format("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s",
-												 this.teamName,this.teamNumber, this.drive, this.cross, this.scoreHigh, this.scoreLow,
-												 this.portcullis, this.cdf, this.moat, this.drawBridge, this.roughTerrain, this.rockWall,
+		return String.format("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s",
+												 this.teamName,this.teamNumber, this.finalScore, this.drive, this.cross, this.scoreHigh, this.scoreLow,
+												 this.portcullis, this.cdf, this.ramparts, this.moat, this.drawBridge, this.roughTerrain, this.rockWall,
 												 this.lowBar, this.highGoalAttempts, this.highGoalScoring, this.lowGoalAttempts,
 												 this.lowGoalScoring, this.didScale);
 
 	}
 	public static void main(String[] args){
 	  boolean eh[] = {true, true, true, true};
-	  int nums[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+	  int nums[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2};
 		StandEntry entry = new StandEntry("Voltage", "386", eh, nums, true);
 		System.out.println(entry);
 	}
